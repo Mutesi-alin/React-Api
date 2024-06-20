24202555be2fa483cc68337a999005f3768b3cec
Mutesi-alin <alinemutesiii@gmail.com>
2024-06-20 08:47:30 +0300

    add file

---
+++ src/About/index.js
@@ -0,0 +1,14 @@
+import './index.css'
+const About= ()=>{
+    return(
+        <div className="about">
+            <p>I am learning ReactJs which is Javascript Front-end libray </p>
+           
+            
+        </div>
+    )
+}
+export default About;
+
+
+
